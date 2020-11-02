731d27540347ae20e5d5f39333fb699abba2ba5b
zhaozf <mdzxzhao@163.com>
2020-11-02 12:44:40 +0800

    设计模式第一次提交

---
+++ src/main/java/com/zzf/singleton/dp_01.java
@@ -0,0 +1,10 @@
+package com.zzf.singleton;
+
+public class dp_01 {
+
+    public static void main(String[] args) {
+        // Simple Singleton
+
+        System.out.println("Hello World!");
+    }
+}
